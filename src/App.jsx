2f97e870f1cd8461e9eff700508bfce913df160c
sai babu <saibabu@sais-MacBook-Air-3.local>
2024-03-10 21:31:26 +0530

    Add.HomeComponent is added with nav bar and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 
 import './App.css'
-import CreateComponent from './components/CreateComponent/CreateComponent';
 import HomeComponent from './components/HomeComponent/HomeComponent';
-import PublishComponent from './components/PublishComponent/PublishComponent';
 
 function App() {
   return (
     <>
      <HomeComponent/>
-     <CreateComponent/>
-     <PublishComponent/>
     </>
   )
 }
--- src/components/HomeComponent/HomeComponent.jsx
+++ src/components/HomeComponent/HomeComponent.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import './HomeComponent.css'
+import CreateComponent from '../CreateComponent/CreateComponent'
+import PublishComponent from '../PublishComponent/PublishComponent'
 function HomeComponent() {
   return (
-    <div>HomeComponent</div>
+    <div>
+        <div className="nav">
+            <div className="nav-left">Blogo🏃🏻</div>
+            <div className="nav-right">Sign In</div>
+        </div>
+        <CreateComponent/>
+        <PublishComponent/>
+    </div>
   )
 }
 
